72164d2630066806ade3f026c1e2dba1cd201320
roenoe <roe@nottseter.no>
2025-06-17 22:23:00 +0200

    nvim command window + fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,39 @@
+set fish_greeting
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Environment variables
+set -x PATH $PATH $HOME/.local/bin/  # Must come first because it alters $PATH
+set -x TERM xterm-256color
+set -x EDITOR nvim
+set -x HOSTNAME (uname -n)
+
+# Get MACHINAHOSTNAME from file or fallback
+if test -f /etc/machinahostname
+    set -x MACHINAHOSTNAME "["(cut -f1-3 -d. < /etc/machinahostname)"]"
+else
+    set -x MACHINAHOSTNAME (uname -n)
+end
+
+# Run oh-my-posh
+eval "$(oh-my-posh init fish --config $HOME/.config/ohmyposh/minimal.toml)"
+
+# Set up fzf key bindings
+fzf --fish | FZF_ALT_C_COMMAND= FZF_CTRL_T_COMMAND= source
+
+# Aliases
+## Nvim
+alias vi nvim
+alias vim nvim
+alias vimdiff "nvim -d"
+## Eza
+alias ls "exa --icons=always -ah --group-directories-first"
+alias ll "ls -l --git"
+alias lsa "ls -l --total-size"
+## Other
+alias .. "cd .."
+alias fetch "fastfetch --config ~/.config/fastfetch/autostart.jsonc"
+alias rg "rg -i --hidden"
+
+fetch
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -822,6 +822,21 @@ require('lazy').setup({
       vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
+  { -- floating command line
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },

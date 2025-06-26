6506555fa7618548846b345c0d50ec37614a94c8
roenoe <roe@nottseter.no>
2025-06-26 23:47:25 +0200

    multiple starship configs
    add multiple starship configs. Need to reference them in zsh if I want
    to use starship with zsh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,6 +24,7 @@ end
 #eval "$(oh-my-posh init fish --config $HOME/.config/ohmyposh/minimal.toml)"
 
 # Run starship
+set -x STARSHIP_CONFIG $HOME/.config/starship/pure.toml
 function starship_transient_prompt_func
   tput cuu1
   starship module character

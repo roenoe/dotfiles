21cbd0f7225752611db66d08d66ca05cf150b8fd
roenoe <roe@nottseter.no>
2025-08-06 13:15:23 +0200

    changed back to swayidle and swaylock for sway

---
+++ .config/sway/scripts/swayidle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#LOCKUTIL=hyprlock
+LOCKUTIL=swaylock
+#LOCK="pidof hyprlock || hyprlock"
+LOCK="pidof swaylock || sh ~/.config/sway/scripts/swaylock.sh"
+SLEEP="pidof $LOCKUTIL && (hyprctl dispatch dpms off || swaymsg 'output * dpms off')"
+UNSLEEP="hyprctl dispatch dpms on || swaymsg 'output * dpms on'"
+
+swayidle -w \
+  timeout 10 "$SLEEP" \
+  resume "$UNSLEEP" \
+  \
+  timeout 570 "notify-send 'Your display is about to go to sleep'" \
+  \
+  timeout 600 "$LOCK && $SLEEP" \
+  resume "$UNSLEEP"

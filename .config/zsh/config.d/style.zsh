ba9333aa852c1de6a35174cdde8720355ad64bf0
Roe Nøttseter <roenoe@mad.zazu.no>
2025-09-10 15:49:26 +0200

    d /home/roenoe/src/ansible-misc/dotfiles

--- .config/zsh/config.d/style.zsh
+++ .config/zsh/config.d/style.zsh
@@ -3,6 +3,7 @@
 # Completion styling
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}' # Case sensitivity
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}" # Show colours in path
+zstyle ':completion:*' special-dirs true
 #zstyle ':completion:*:default' menu select=2 # Shows box on completions
 
 # Fzf completion styling

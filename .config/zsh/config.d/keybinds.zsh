477c04f6f735d40f0ac465a8484e84ba857b3794
Roe Nøttseter <roe@machina.no>
2025-11-10 15:01:28 +0100

    add . to WORDLIST so that esc backspace works better with ssh

--- .config/zsh/config.d/keybinds.zsh
+++ .config/zsh/config.d/keybinds.zsh
@@ -3,6 +3,11 @@
 # Emacs mode in prompt
 bindkey -e
 
+# Add edit current command in $EDITOR
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^X^E" edit-command-line
+
 # Ctrl + right left arrow
 bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
@@ -10,7 +15,7 @@ bindkey "^[[3~" delete-char
 
 # Esc + backspace bash parity
 backward-kill-dir () {
-    local WORDCHARS=${WORDCHARS/\/}
+    local WORDCHARS=${${WORDCHARS/\/}/.}
     zle backward-kill-word
     zle -f kill
 }
